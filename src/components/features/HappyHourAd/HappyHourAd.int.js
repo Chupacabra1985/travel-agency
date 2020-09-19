9d2103e5c684bb8041c736c76671dd8f0e1c5d39
Jan Roztropinski <jan.roztropinski@twino.pl>
2020-09-19 23:16:01 +0200

    feature(task) Implementacja funkcji formatTime

--- src/components/features/HappyHourAd/HappyHourAd.int.js
+++ src/components/features/HappyHourAd/HappyHourAd.int.js
@@ -13,28 +13,6 @@ const mockProps = {
   description: 'Promotion start',
 };
 
-beforeAll(() => {
-  const utilsModule = jest.requireActual('../../../utils/formatTime.js');
-  utilsModule.formatTime = jest.fn(seconds => seconds);
-});
-
-describe('Component HappyHourAd', () => {
-  it('should render without crashing', () => {
-    const component = shallow(<HappyHourAd />);
-    expect(component).toBeTruthy();
-  });
-  it('should render heading and description', () => {
-    const component = shallow(<HappyHourAd />);
-    expect(component.exists(select.title)).toEqual(true);
-    expect(component.exists(select.promoDescription)).toEqual(true);
-  });
-  it('should render title', () => {
-    const title = 'Promo title';
-    const component = shallow(<HappyHourAd {...mockProps}/>);
-    expect(component.find('h3').text()).toEqual(title);
-  });
-});
-
 const trueDate = Date;
 const mockDate = customDate => class extends Date {
   constructor(...args) {
@@ -88,7 +66,4 @@ describe('Component HappyHourAd with mocked Date and delay', () => {
   checkDescriptionAfterTime('11:57:58', 2, '120');
   checkDescriptionAfterTime('11:59:58', 1, '1');
   checkDescriptionAfterTime('13:00:00', 60 * 60, 22 * 60 * 60 + '');
-  checkDescriptionAtTime('12:00:00', 'Promotion start');
-  checkDescriptionAtTime('12:59:59', 'Promotion start');
-  checkDescriptionAfterTime('11:59:58', 2, 'Promotion start');
 });
--- src/components/features/HappyHourAd/HappyHourAd.js
+++ src/components/features/HappyHourAd/HappyHourAd.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './HappyHourAd.scss';
 import PropTypes from 'prop-types';
+import {formatTime} from '../../../utils/formatTime';
 
 class HappyHourAd extends React.Component {
 
@@ -27,7 +28,7 @@ class HappyHourAd extends React.Component {
     if(time > 82800) {
       descr = description;
     } else {
-      descr = time;
+      descr = formatTime(time);
     }
     return(
       <div className={styles.component}>
